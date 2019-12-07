a7d95f2b8b882ce98ef64e730c17ff16230455e0
lichunhua <lichunhua@zto.com>
2019-12-07 23:47:08 +0800

    update redis

--- java/startup.sh
+++ java/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "export JAVA_HOME=/usr/local/java/current" >> /etc/profile.d/env
+echo "export JAVA_HOME=/apps/svr/java/current" >> /etc/profile.d/env
 echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile.d/env
 
 source /etc/profile.d/env
